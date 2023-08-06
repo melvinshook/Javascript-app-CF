639b0fa68f3311e194b59a11ef679309854347af
melvinshook <139077191+melvinshook@users.noreply.github.com>
2023-08-06 19:23:43 -0400

    changed format and added for loop
    added a for loop to go over each item in the pokemonList and then used document.write for the DOM to display the information

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
-let pokemonList = [{ name: 'balbasaur', height: 3, type: 'grass' },
-    { name: 'charizard', height: 10, type: 'fire' },
-    { name: 'zapdos', height: 9, type: ['electric', 'flying'] },
-    { name: 'hitmonchan', height: 6, type: 'fighting'}]
+let pokemonList = [
+    {
+        name: 'balbasaur',
+        height: 0.7,
+        type: 'grass'
+    },
+    {
+        name: 'charizard',
+        height: 1.7,
+        type: 'fire'
+    },
+    {
+        name: 'zapdos',
+        height: 1.6,
+        type: ['electric', 'flying']
+    },
+    {
+        name: 'hitmonchan',
+        height: 1.4,
+        type: 'fighting'
+    }
+];
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1) {
+        document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that’s big!`)
+    } else {
+        document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) `)
+    }
+}
+
