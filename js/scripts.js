9940c09ba741b166cf423f2e56ea952e588c42e6
melvinshook <139077191+melvinshook@users.noreply.github.com>
2023-08-13 15:24:35 -0400

    document.write different syntax

--- js/scripts.js
+++ js/scripts.js
@@ -21,12 +21,11 @@ let pokemonList = [
     }
 ];
 
-
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1) {
-        document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that’s big!`)
+        document.write("<p>" + pokemonList[i].name + " " + "(Height:" + pokemonList[i].height + ")" + "Wow, that\'s big" + "</p>");
     } else {
-        document.write(` ${pokemonList[i].name} (height: ${pokemonList[i].height}) `)
+        document.write("<p>" + pokemonList[i].name + " " + "(Height:" + pokemonList[i].height + ")" + "</p>");
     }
 }
 
