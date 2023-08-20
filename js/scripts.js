decc31ab01177fbca4c504d648252f1f301601e7
melvinshook <139077191+melvinshook@users.noreply.github.com>
2023-08-19 21:46:25 -0400

    add pokemonRepository
    added an add and getAll funtction, IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,47 @@
-let pokemonList = [
-    {
-        name: 'balbasaur',
-        height: 0.7,
-        type: 'grass'
-    },
-    {
-        name: 'charizard',
-        height: 1.7,
-        type: 'fire'
-    },
-    {
-        name: 'zapdos',
-        height: 1.6,
-        type: ['electric', 'flying']
-    },
-    {
-        name: 'hitmonchan',
-        height: 1.4,
-        type: 'fighting'
-    }
-];
+let pokemonRepository = (function () {
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1) {
-        document.write("<p>" + pokemonList[i].name + " " + "(Height:" + pokemonList[i].height + ")" + "Wow, that\'s big" + "</p>");
-    } else {
-        document.write("<p>" + pokemonList[i].name + " " + "(Height:" + pokemonList[i].height + ")" + "</p>");
+    let pokemonList = [
+        {
+            name: 'balbasaur',
+            height: 0.7,
+            type: 'grass'
+        },
+        {
+            name: 'charizard',
+            height: 1.7,
+            type: 'fire'
+        },
+        {
+            name: 'zapdos',
+            height: 1.6,
+            type: ['electric', 'flying']
+        },
+        {
+            name: 'hitmonchan',
+            height: 1.4,
+            type: 'fighting'
+        }
+    ];
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-}
 
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Squirtle', height: 1.8, type: 'Water' });
+console.log(pokemonRepository.getAll());
+
+
+Object.keys(pokemonList).forEach(function (poke) {
+    console.log(pokemonList[poke]);
+});
